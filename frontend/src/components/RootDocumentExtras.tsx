2f1cbc21f07eb471e90a8ad17b707758b948eff5
JongYun Jeong <95991290+BellYun@users.noreply.github.com>
2026-05-21 13:03:59 +0900

    feat: AdSense 로더/슬롯/ads.txt 라우트 기반 추가

--- frontend/src/components/RootDocumentExtras.tsx
+++ frontend/src/components/RootDocumentExtras.tsx
@@ -1,5 +1,6 @@
 import { Analytics } from "@vercel/analytics/next";
 import Script from "next/script";
+import { AdSenseScript } from "@/components/ads/AdSenseScript";
 import { AmplitudeLoader } from "@/components/AmplitudeLoader";
 import { GoogleAnalytics } from "@/components/GoogleAnalytics";
 import { WebVitalsReporter } from "@/components/WebVitalsReporter";
@@ -11,6 +12,7 @@ export function RootDocumentExtras() {
       <AmplitudeLoader />
       <WebVitalsReporter />
       <GoogleAnalytics />
+      <AdSenseScript />
       {process.env.NEXT_PUBLIC_SENTRY_DSN && (
         <Script
           src={`https://js.sentry-cdn.com/${process.env.NEXT_PUBLIC_SENTRY_DSN.match(/\/\/(.+?)@/)?.[1]}.min.js`}
